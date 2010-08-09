9562924a7a5095d6e51ba746ce7e26ea36aee7f7
Markuz Lindgren <markuz.lindgren@iteraconsulting.com>
2010-08-09 19:10:14 +0200

    Revert "Removed extra unnecessary Map view. Everything is now included in the ClubZone tab, in the main tabbar"
    This reverts commit 405b62e447928e637dedbebe22146fb308532731.

--- Classes/ClubZoneViewController.h
+++ Classes/ClubZoneViewController.h
@@ -7,21 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MapKit/MapKit.h>
-#import <QuartzCore/QuartzCore.h>
-#import "ClubzoneMapAnnotation.h"
 
-@interface ClubZoneViewController : UIViewController <MKMapViewDelegate>{
-	IBOutlet MKMapView *mapView;
-	IBOutlet UIButton *locationToggle;
-	BOOL followingLocation;
+
+@interface ClubZoneViewController : UIViewController {
+
 }
 
-@property (nonatomic, retain) MKMapView *mapView;
-@property (nonatomic, retain) UIButton *locationToggle;
-
-
-- (IBAction)locationButton:(id)sender;
-- (void) goToDefaultLocationAndZoom;
+- (IBAction)openMap:(id)sender;
 
 @end
